4db06064aa70ab8f92aef199d0dd2a41a7ad0560
Leonid Krainichuk <lonakrajnicuk@gmail.com>
2026-07-09 18:10:55 +0300

    Add files via upload

---
+++ Курсач/Fandom_copy/Fandom_copy/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Fandom_copy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fandom_copy.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
