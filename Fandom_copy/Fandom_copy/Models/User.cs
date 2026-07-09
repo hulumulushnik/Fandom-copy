d1b8fe1859972954a1effec253ed57dbf34a5e53
Leonid Krainichuk <lonakrajnicuk@gmail.com>
2026-07-09 18:27:23 +0300

    Add files via upload

---
+++ Fandom_copy/Fandom_copy/Models/User.cs
@@ -0,0 +1,25 @@
+﻿namespace Fandom_copy.Models
+{
+    class User
+    {
+        Guid Id;
+
+        string Login;
+
+        string Email;
+
+        string PasswordHash;
+
+        GlobalRole GlobalRole;
+
+        DateTime RegistrationDate;
+
+        bool IsBanned;
+    }
+
+    enum GlobalRole
+    {
+        User,
+        Admin
+    }
+}
